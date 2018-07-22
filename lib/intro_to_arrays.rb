252b30d1e30b6e3b5e393005e3c1048d3249d31f
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-22 02:07:25 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  return Array.new
+end
+
+def array_with_two_elements
+  return [1,2]
+end
+
+def first_element(a)
+  return a[0]
+end
+
+def third_element(a)
+  return a[2]
+end 
+
+def last_element(a)
+  return a[-1]
+end
+
+def first_element_with_array_methods(a)
+  return a.first
+end
+
+def last_element_with_array_methods(a)
+  return a.last
+end
+
+def length_of_array(a)
+  return a.length
+end
+
